42bb1b6ae10364e54c743191cbb3e16ca9a28ec1
Olivier Chafik <ochafik@users.noreply.github.com>
2023-08-11 13:48:58 +0100

    Update example.py w/ auto dtype

--- examples/python/example.py
+++ examples/python/example.py
@@ -1,4 +1,4 @@
-from ggml import GgmlContext, GgmlTensor, lib
+from ggml import GgmlContext, GgmlTensor, lib, type_to_dtype
 import numpy as np
 
 if __name__ == '__main__':
@@ -11,18 +11,20 @@ if __name__ == '__main__':
     # type = lib.GGML_TYPE_I8
     # type = lib.GGML_TYPE_Q4_0
     # type = lib.GGML_TYPE_Q4_K
-    # type = lib.GGML_TYPE_F16
-    type = lib.GGML_TYPE_F32
+    type = lib.GGML_TYPE_F16
+    # type = lib.GGML_TYPE_F32
+    dtype = type_to_dtype[type]
+
     a = ctx.new_tensor(type, n, n)
     b = ctx.new_tensor(type, n, n)
     # print(f'a: shape={a.shape}, strides={a.strides}')
     # print(f'b: shape={b.shape}, strides={b.strides}')
 
     # a.write([(i) for i in range(n * n)])
-    a.write(np.ones((n, n), dtype=np.float32), quantize=True)
+    a.write(np.ones((n, n), dtype=dtype), quantize=True)
     # b.write([(i * 2) for i in range(n * n)])
-    b.write(np.random.rand(n, n).astype(np.float32), quantize=True)
-    # b.write(np.ones((n, n), dtype=np.float32))
+    b.write(np.random.rand(n, n).astype(dtype), quantize=True)
+    # b.write(np.ones((n, n), dtype=dtype))
 
     # c = a * b
     c = a + b
