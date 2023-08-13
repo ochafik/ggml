396fd700c8f8e64aa277cb2a321810ab560be0d4
ochafik <ochafik@google.com>
2023-08-13 13:39:40 +0100

    Better stubs w/ pycparser instead of regexps

--- examples/python/stubs_generator.py
+++ examples/python/stubs_generator.py
@@ -3,81 +3,102 @@
 
   See the various environment variables at the top of this file for options.
 """
+import sys
 
+# This is not required if you've installed pycparser into
+# your site-packages/ with setup.py
+sys.path.extend(['.', '..'])
+
+import itertools
 import re
-from sys import argv
 
-def to_type(t):
-  """
-    from a C type, returns the corresponding Python type, or None if unknown.
-  """
-  t = t.strip()
-  if t.endswith('*'):
-    return 'ffi.CData'
-  elif t.startswith('struct '):
-    return 'ffi.CData'
-  elif t.startswith('enum ') or t in ['int', 'int64_t', 'size_t', 'int32_t', 'int16_t', 'int8_t', 'uint64_t', 'uint32_t', 'uint16_t', 'uint8_t']:
-    return 'int'
-  elif t in ['float', 'double']:
-    return 'float'
-  elif t == 'void':
-    return 'None'
-  elif t == 'bool':
-    return 'bool'
-  print('## Unknown type:', t)
-  return None
+from pycparser import c_ast, parse_file, CParser
+import pycparser.plyparser
+from pycparser.c_ast import PtrDecl, TypeDecl, FuncDecl, EllipsisParam, IdentifierType, Struct, Enum
 
-def format_arg(t, n):
-  t = to_type(t)
-  return n if t is None else f'{n}: {t}'
+__c_type_to_python_type = {
+    'void': 'None', '_Bool': 'bool',
+    'char': 'int', 'short': 'int', 'int': 'int', 'long': 'int',
+    'ptrdiff_t': 'int', 'size_t': 'int',
+    'int8_t': 'int', 'uint8_t': 'int',
+    'int16_t': 'int', 'uint16_t': 'int',
+    'int32_t': 'int', 'uint32_t': 'int',
+    'int64_t': 'int', 'uint64_t': 'int',
+    'float': 'float', 'double': 'float',
+    'ggml_fp16_t': 'np.float16',
+}
 
-def format_ret(t):
-  t = to_type(t)
-  return '' if t is None else f' -> {t}'
+def format_type(t: TypeDecl):
+    if isinstance(t, PtrDecl) or isinstance(t, Struct):
+        return 'ffi.CData'
+    if isinstance(t, Enum):
+        return 'int'
+    if isinstance(t, TypeDecl):
+        return format_type(t.type)
+    if isinstance(t, IdentifierType):
+        assert len(t.names) == 1, f'Expected a single name, got {t.names}'
+        name = t.names[0]
+        return __c_type_to_python_type.get(name) or 'ffi.CData'
+    return t.name
+
+class PythonStubFuncDeclVisitor(c_ast.NodeVisitor):
+    def __init__(self):
+        self.sigs = {}
+        self.sources = {}
+
+    def get_source_snippet_lines(self, coord: pycparser.plyparser.Coord) -> list[str]:
+        if coord.file not in self.sources:
+            with open(coord.file, 'rt') as f:
+                self.sources[coord.file] = f.readlines()
+        source_lines = self.sources[coord.file]
+        ncomments = len(list(itertools.takewhile(lambda i: re.search(r'^\s*(//|/\*)', source_lines[i]), range(coord.line - 2, -1, -1))))
+        lines = []
+        for line in source_lines[coord.line - 1 - ncomments:]:
+            lines.append(line.rstrip())
+            if (';' in line) or ('{' in line): break
+        return lines
+
+    def visit_Enum(self, node: Enum):
+        if node.values is not None:
+          for e in node.values.enumerators:
+              self.sigs[e.name] = f'  @property\n  def {e.name}(self) -> int: ...'
+
+    def visit_FuncDecl(self, node: FuncDecl):
+        tp = node.type
+        is_ptr = False
+        while isinstance(tp, PtrDecl):
+            tp = tp.type
+            is_ptr = True
+        
+        name = tp.declname
+        if name.startswith('__'):
+            return
+        
+        args = []
+        for a in node.args.params:
+            if isinstance(a, EllipsisParam):
+                args.append('*args')
+            elif a.name is not None:
+                args.append(f'{a.name}: {format_type(a.type)}')
+        ret = format_type(tp if not is_ptr else node.type)
+
+        self.sigs[name] = '\n'.join([
+            *[f'  # {l}' for l in self.get_source_snippet_lines(node.coord)],
+            f'  def {name}({", ".join(args)}) -> {ret}: ...',
+        ])
 
 def generate_stubs(header: str):
     """
-      Hack to generates a .pyi Python stub file for the GGML API using C header files.
-
-      The idea is to remove comments, then unbreak functions declared across multiple lines,
-      then pseudo-parse the function name, return type, args and their types. Simple yet efficient.
-
-      I might contribute a cleaner version of this to cffi at some point, using the
-      actual AST it already has.
+      Generates a .pyi Python stub file for the GGML API using C header files.
     """
-    # Remove comments and ~ensure each GGML_API function signature is on a single line
-    header = re.sub(r'/\*.*?\*/', '', header, flags=re.M | re.S)
-    header = re.sub(r'//.*([\n\r])', '\\1', header)
-    header = re.sub('([(,])\\s*[\\n\\r]\\s*', r'\1', header, flags=re.M | re.S)
-    header = re.sub(r'[ \t]+', ' ', header)
-    header = re.sub(r'\(\s*void\s*\)', '()', header)
 
-    sigs = list(set([l.strip() for l in header.splitlines() if 'GGML_API' in l and 'GGML_DEPRECATED' not in l]))
-    sigs.sort()
+    with open('stubs.h', 'wt') as f:
+        f.write(header)
 
-    # The cffi binding will expose an object `lib` with all the constants and functions.
-    # We stub it using a Python class with static methods, which works w/ at least pylance / VS Code.
-    lib = ['class lib:']
-    for sig in sigs:
-        m = re.search(r'GGML_API\s*(.*?)\b(\w+)\s*\(([^)]*)\)\s*;', sig)
-        if m is not None:
-            try:
-                (rettype, name, arglist) = m.groups()
-                arglist = [a.strip() for a in arglist.split(',')]
-                
-                args = []
-                for arg in arglist:
-                    if arg.strip() == '':
-                        continue
-                    if arg == '...':
-                        args.append('*args')
-                    else:
-                        am = re.search(r'^(.+?)\b(\w+)$', arg)
-                        args.append(format_arg(am.group(1), am.group(2)))
-                lib += [f'  # {sig}', 
-                        f'  def {name}({", ".join(args)}){format_ret(rettype)}: ...',
-                        '']
-            except:
-                print('## Error parsing:', sig)
-                raise
-    return '\n'.join(lib)
+    v = PythonStubFuncDeclVisitor()
+    v.visit(CParser().parse(header, "<input>"))
+
+    keys = list(v.sigs.keys())
+    keys.sort()
+
+    return '\n'.join(['class lib:', *[v.sigs[k] for k in keys]])
